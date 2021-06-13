16c38222c3d297441cd73f3688ea34c3fdb0acdb
Faithfulness Alamu <vaguemail369@gmail.com>
2021-06-13 09:25:32 +0100

    feat(data): create a data store interface

---
+++ internal/data/store.go
@@ -0,0 +1,21 @@
+package data
+
+import "github.com/thealamu/taskboard/internal/entity"
+
+type Store interface {
+	AddTask(entity.Task) error
+
+	// GetTasks returns tasks. If all is true, completed tasks are returned too.
+	GetTasks(all bool) ([]entity.Task, error)
+
+	DeleteTask(id int) error
+
+	// ToggleTask checks a task if it is unchecked, unchecks a task if it is checked.
+	ToggleTask(id int) error
+
+	UpdateTask(entity.Task) error
+
+	// FindTasks returns tasks with the filter in their description.
+	// If all is true, completed tasks are searched too.
+	FindTasks(filter string, all bool) ([]entity.Task, error)
+}
