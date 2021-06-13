6250bf4755e105d250d4f7249b95160068bae592
Faithfulness Alamu <vaguemail369@gmail.com>
2021-06-13 10:11:59 +0100

    feat(data): create a sql data store
    Create a sql data store with an underlying sqlite database.
    This store implements the data.Store interface.
    
      - create table if it does not exist
      - implement task insertion
      - implement retrieving tasks
      - implement task deletion
      - implement task checking
      - implement task updates
      - implement task searching

---
+++ internal/data/sql/store.go
@@ -0,0 +1,131 @@
+package sql
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
+	"github.com/thealamu/taskboard/internal/entity"
+)
+
+type SQLStore struct {
+	db *sql.DB
+}
+
+func NewStore() (*SQLStore, error) {
+	db, err := newSqliteDB()
+	if err != nil {
+		return nil, err
+	}
+	createStoreTable(db)
+	return &SQLStore{db}, nil
+}
+
+func (s *SQLStore) AddTask(t entity.Task) error {
+	qry := `INSERT INTO tasks(checked, description, date) VALUES (?, ?, ?)`
+	_, err := s.db.Exec(qry, t.Checked, t.Description, t.Date.Unix())
+	return err
+}
+
+// GetTasks returns tasks. If all is true, completed tasks are returned too.
+func (s *SQLStore) GetTasks(all bool) ([]entity.Task, error) {
+	qry := `SELECT * FROM tasks`
+	if !all {
+		qry += ` WHERE checked=false`
+	}
+	rows, err := s.db.Query(qry)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	return readTasksFromRows(rows)
+}
+
+func (s *SQLStore) DeleteTask(id int) error {
+	qry := `DELETE FROM tasks WHERE id=?`
+	_, err := s.db.Exec(qry, id)
+	return err
+}
+
+// ToggleTask checks a task if it is unchecked, unchecks a task if it is checked.
+func (s *SQLStore) ToggleTask(id int) error {
+	qry := `UPDATE tasks SET checked= ((checked | 1) - (checked & 1)) WHERE id=?`
+	_, err := s.db.Exec(qry, id)
+	return err
+}
+
+func (s *SQLStore) UpdateTask(t entity.Task) error {
+	qry := `UPDATE tasks SET checked=?, description=?, date=? WHERE id=?`
+	_, err := s.db.Exec(qry, t.Checked, t.Description, t.Date.Unix(), t.ID)
+	return err
+}
+
+// FindTasks returns tasks with the filter in their description.
+// If all is true, completed tasks are searched too.
+func (s *SQLStore) FindTasks(filter string, all bool) ([]entity.Task, error) {
+	qry := `SELECT * FROM tasks WHERE description LIKE '%' || ? || '%'`
+	if !all {
+		qry += ` AND checked=false`
+	}
+	rows, err := s.db.Query(qry, filter)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	return readTasksFromRows(rows)
+}
+
+func readTasksFromRows(rows *sql.Rows) ([]entity.Task, error) {
+	var tasks []entity.Task
+
+	// loop through result rows, inserting a task into tasks each iteration
+	for rows.Next() {
+		t := entity.Task{}
+		var timeInt int64
+
+		err := rows.Scan(&t.ID, &t.Checked, &t.Description, &timeInt)
+		if err != nil {
+			return nil, err
+		}
+
+		t.Date = time.Unix(timeInt, 0)
+		tasks = append(tasks, t)
+	}
+
+	err := rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return tasks, nil
+}
+
+var _DSN = `file:taskboard.db`
+
+func newSqliteDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", _DSN)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, errors.Wrap(err, "could not connect to sqlite database")
+	}
+	return db, nil
+}
+
+func createStoreTable(db *sql.DB) error {
+	schema := `
+		CREATE TABLE IF NOT EXISTS tasks (
+			id INTEGER PRIMARY KEY,
+			checked INTEGER DEFAULT 0,
+			description TEXT NOT NULL,
+			date INTEGER 
+		)
+	`
+	_, err := db.Exec(schema)
+	if err != nil {
+		return err
+	}
+	return nil
+}
