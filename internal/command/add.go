c1cb1d7b79ffaa5344b2506ab611fc5daf3f1ae4
Faithfulness Alamu <vaguemail369@gmail.com>
2021-06-15 10:22:59 +0100

    feat: user can add tasks

--- internal/command/add.go
+++ internal/command/add.go
@@ -1,8 +1,11 @@
 package command
 
 import (
+	"time"
+
 	"github.com/spf13/cobra"
 	"github.com/thealamu/taskboard/internal/data"
+	"github.com/thealamu/taskboard/internal/entity"
 	"github.com/thealamu/taskboard/internal/ui"
 )
 
@@ -18,6 +21,21 @@ func NewAddCommand(view *ui.UI, db data.Store) *cobra.Command {
 }
 
 func addCommand(view *ui.UI, db data.Store) commandFunc {
+	fail := func(task entity.Task, msg string) {
+		view.SetRemark(false, msg, task).Render()
+	}
+
+	success := func(task entity.Task, msg string) {
+		view.SetRemark(true, msg, task).Render()
+	}
+
 	return func(cmd *cobra.Command, args []string) {
+		task := entity.Task{Description: args[0], Date: time.Now()}
+		err := db.AddTask(task)
+		if err != nil {
+			fail(task, err.Error())
+			return
+		}
+		success(task, "Created task")
 	}
 }
